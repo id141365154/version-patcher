ab19b091b6f971cc491af183868bf42e2da78076
Victor Kulyabin <ku@appkode.ru>
2019-08-28 00:01:03 +0200

    compleete

--- bin/version-patcher.js
+++ bin/version-patcher.js
@@ -1,9 +1,6 @@
-#!/usr/bin / env node
+#!/usr/bin/env node
 
 const fs = require("fs");
-const child_process = require("child_process");
-const readline = require("readline");
-const https = require("https");
 const printMessage = require("print-message");
 
 // Makes the script crash on unhandled rejections instead of silently
@@ -13,16 +10,8 @@ process.on("unhandledRejection", err => {
   throw err;
 });
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-
-
-const getByPath = (object, path) => {
+var getByPath = (object, path) => {
   const steps = path.split('.')
-
   if (steps.length === 1) {
     return object[steps]
   }
@@ -34,35 +23,84 @@ const getByPath = (object, path) => {
   }
 }
 
+var setByPath = (object, path, value) => {
+  const steps = path.split('.')
+  if (steps.length === 1) {
+    object[steps] = value
+    return object[steps]
+  }
+  if (steps.length > 1) {
+    const newObj = object[steps[0]]
+    steps.shift()
+    return setByPath(newObj, steps.join('.'), value)
+  }
+}
+
+const updateVersion = (version, versionType) => {
+  version = version + ''
+  const versions = version.split('.').map(item => parseInt(item))
+  if (versions.length === 1) {
+    return parseInt(version) + 1
+  }
+
+  if (versionType === 'patch') {
+    versions[2] = versions[2] + 1
+  }
+  if (versionType === 'minor') {
+    versions[1] = versions[1] + 1
+    versions[2] = 0
+  }
+  if (versionType === 'major') {
+    versions[0] = versions[0] + 1
+    versions[1] = 0
+    versions[2] = 0
+  }
+
+  return versions.join('.')
+}
+
 const confName = "version-patcher.config.json";
 let config = "";
+const versionType = process.argv[process.argv.length - 1]
+
+printMessage(["VERSION PATCHER"]);
 
 try {
   config = fs.readFileSync(confName, "utf8");
 } catch (e) {
-  console.log("\x1b[31m", `Can\`t read config file: ${confName}`);
+  console.error("\x1b[31m", `Can\`t read config file: ${confName}`);
   throw e;
 }
 
 try {
   config = JSON.parse(config);
 } catch (e) {
-  console.log(`Can\`t parse config file: ${confName}`);
+  console.error(`Can\`t parse config file: ${confName}`);
   throw e;
 }
 
-printMessage(["VERSION PATCHER"]);
 
 config.patch.map(item => {
   const patchedFile = fs.readFileSync(item.file, 'utf8')
 
-  let fileObject = JSON.parse(patchedFile)
+  let fileObject = ''
+
+  try {
+    fileObject = JSON.parse(patchedFile)
+  } catch (e) {
+    printMessage(['Can`t parse JSON in ', '', item.file, '', 'Please note that other files may have been patched'], {
+      borderColor: "red"
+    })
+
+    throw e;
+  }
 
   item.params.map(param => {
-    let version = getByPath(fileObject, Object.keys(param)[0])
-    console.log(Object.keys(param)[0] + " version", version);
+    const version = getByPath(fileObject, Object.keys(param)[0])
+    const newVersion = setByPath(fileObject, Object.keys(param)[0], updateVersion(version, versionType))
+    printMessage(['File: ' + item.file, 'Path: ' + Object.keys(param)[0], 'New version: ' + newVersion]);
   })
-
-
-  //  console.log(fileObject)
+  fs.writeFileSync(item.file, JSON.stringify(fileObject, null, '  '), 'utf8');
 })
+
+process.exit()
